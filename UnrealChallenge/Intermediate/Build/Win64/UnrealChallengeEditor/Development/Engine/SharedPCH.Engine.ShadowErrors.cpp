21573a626496df8c25ebefcf4e097f457fe40a09
phipenguu <paul_ong@dlsu.edu.ph>
2022-01-07 18:55:42 +0800

    Revert "PowerUp blueprint (does not work yet)"
    This reverts commit f4d5017c7f5537c9a8cd151832e8c8365f824148.

--- UnrealChallenge/Intermediate/Build/Win64/UnrealChallengeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.27.29112
-#include "C:/Users/Nathan/Documents/GitHub/UnrealChall2/UnrealChallenge/Intermediate/Build/Win64/UnrealChallengeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- UnrealChallenge/Source/UnrealChallenge/UnrealChallenge.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "UnrealChallenge.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealChallenge, "UnrealChallenge" );
